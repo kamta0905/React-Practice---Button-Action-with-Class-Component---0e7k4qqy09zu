d007574c39d6c3a490b449dc0d7644d2553da4d9
kamta0905 <88268903+kamta0905@users.noreply.github.com>
2021-08-27 00:12:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,20 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	     this.state = {
+            pHidden: true,
+	     }
 	};
 
     render() {
     	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
+    	  <div id="main">
+                <button id="click" onClick={() => {
+                    this.setState({ pHidden: false })
+                }}>Show Para</button>
+                {this.state.pHidden ? null : (
+                    <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}
+            </div>
     	);
     }
 }
